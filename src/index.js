cc580a8be8254d5b87c2975f1a951d746994adc5
thuno <thuno2698@gmail.com>
2024-07-03 11:33:30 +0700

    update 0.3.0

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './skin/color.css'
+import './skin/layoout.css'
+import './skin/typography.css'
+import { Checkbox } from './component/checkbox/checkbox.tsx'
+import { Select1 } from './component/select1/select1.tsx'
+import { Switch } from './component/switch/switch.tsx'
+import { showPopup, closePopup, Popup } from './component/popup/popup.tsx'
+import { showDialog, Dialog, DialogAlignment } from './component/dialog/dialog.tsx'
+import { DatePicker } from './component/date-picker/date-picker.tsx'
+import { SelectMultiple } from './component/input-multi-select/input-multi-select.tsx'
+import { ProgressBar } from './component/progress-bar/progress-bar.tsx'
+import { ComponentStatus, getStatusIcon } from './component/component-status.tsx'
+import { Text } from './component/text/text.tsx'
+import { Pagination } from './component/pagination/pagination.tsx'
+import { Table, TbCell, TbHeader, TbRow, TbBody, CellAlignItems } from './component/table/table.tsx'
+import { TextField } from './component/text-field/text-field.tsx'
+import { RadioButton } from './component/radio-button/radio-button.tsx'
+import { TextArea } from './component/text-area/text-area.tsx'
+import { ImportFile } from './component/import-file/import-file.tsx'
+import { ToastMessage } from './component/toast-noti/toast-noti.tsx'
+import { Calendar, CalendarType } from './component/calendar/calendar'
+import { InfiniteScroll } from './component/infinite-scroll/infinite-scroll.tsx'
+import { Rating } from './component/rating/rating.tsx'
+import { ProgressCircle } from './component/progress-circle/progress-circle.tsx'
+import { CustomSlider } from './component/slider/slider.tsx'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export {
+  Calendar, CalendarType,
+  ComponentStatus,
+  getStatusIcon,
+  Checkbox,
+  Select1,
+  Switch,
+  Popup, showPopup, closePopup,
+  Dialog, showDialog, DialogAlignment,
+  DatePicker,
+  SelectMultiple,
+  ProgressBar,
+  Text,
+  Pagination,
+  Table, TbCell, TbHeader, TbBody, TbRow, CellAlignItems,
+  TextField,
+  RadioButton,
+  TextArea,
+  ImportFile,
+  ToastMessage,
+  InfiniteScroll,
+  Rating,
+  ProgressCircle,
+  CustomSlider,
+}
