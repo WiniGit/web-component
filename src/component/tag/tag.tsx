d5ccf92720a824a55a4c0a512a4e0c7fc4674d2a
thuno <thuno2698@gmail.com>
2024-10-21 17:19:49 +0700

    Merge branch 'main' into chienv

---
+++ src/component/tag/tag.tsx
@@ -0,0 +1,29 @@
+import React, { CSSProperties, ReactNode } from 'react';
+import { Text } from '../text/text';
+import styles from './tag.module.css'
+import { ComponentStatus } from '../component-status';
+
+interface TagProps {
+    id?: string,
+    title: string,
+    prefix?: ReactNode,
+    suffix?: ReactNode,
+    disabled?: boolean,
+    style?: CSSProperties,
+    className?: string,
+    status: ComponentStatus,
+    onClick?: React.MouseEventHandler<HTMLDivElement>
+}
+
+export class Tag extends React.Component<TagProps> {
+
+    render() {
+        return <div id={this.props.id} tag-type={this.state ?? ComponentStatus.INFOR} className={`${styles['tag-container']} row ${this.props.onClick ? styles['type-button'] : ''} ${this.props.disabled ? styles['disabled'] : ""} ${this.props.className ?? "button-text-2"} `} style={this.props.style} onClick={this.props.onClick}>
+            {this.props.prefix}
+            <Text maxLine={1} className={styles['tag-label']}>{this.props.title}</Text>
+            {this.props.suffix}
+        </div>
+    }
+}
+
+
--- src/index.tsx
+++ src/index.tsx
@@ -24,7 +24,6 @@ import { Rating } from './component/rating/rating'
 import { ProgressCircle } from './component/progress-circle/progress-circle'
 import { CustomSlider } from './component/slider/slider'
 import { ToastContainer } from 'react-toastify'
-import { CardInfo } from './component/card/card'
 import { Button } from './component/button/button'
 
 
@@ -33,8 +32,7 @@ export {
   ComponentStatus,
   getStatusIcon,
   Checkbox,
-  Select1, OptionsItem,
-  Switch,
+  Select1, Switch,
   Popup, showPopup, closePopup,
   Dialog, showDialog, DialogAlignment,
   DatePicker,
@@ -53,6 +51,6 @@ export {
   ProgressCircle,
   CustomSlider,
   ToastContainer,
-  CardInfo,
   Button
 }
+export type { OptionsItem }
