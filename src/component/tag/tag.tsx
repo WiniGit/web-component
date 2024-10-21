17b5648df3e4ae232e9e11ac85078ebe874ffb9f
ChienNV20 <chiennv20@fpt.com>
2024-10-21 16:53:50 +0700

    done tag

--- src/component/tag/tag.tsx
+++ src/component/tag/tag.tsx
@@ -1,6 +1,7 @@
 import React, { CSSProperties, ReactNode } from 'react';
 import { Text } from '../text/text';
 import styles from './tag.module.css'
+import { ComponentStatus } from '../component-status';
 
 interface TagProps {
     id?: string,
@@ -10,16 +11,18 @@ interface TagProps {
     disabled?: boolean,
     style?: CSSProperties,
     className?: string,
-    onClick?: React.MouseEventHandler<HTMLButtonElement>
+    status: ComponentStatus,
+    onClick?: React.MouseEventHandler<HTMLDivElement>
 }
 
 export class Tag extends React.Component<TagProps> {
+
     render() {
-        return <button id={this.props.id} type={"button"} disabled={this.props.disabled} className={`${styles['tag-container']} row ${this.props.className ?? "button-text-3"}`} style={this.props.style} onClick={this.props.onClick}>
+        return <div id={this.props.id} tag-type={this.state ?? ComponentStatus.INFOR} className={`${styles['tag-container']} row ${this.props.disabled ? styles['disabled'] : ""} ${this.props.className ?? "button-text-2"} `} style={this.props.style} onClick={this.props.onClick}>
             {this.props.prefix}
             <Text maxLine={1} className={styles['tag-label']}>{this.props.label}</Text>
             {this.props.suffix}
-        </button>
+        </div>
     }
 }
 
