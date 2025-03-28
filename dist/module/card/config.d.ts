8c21ab5290c27db420415fbc18b49c57a1c8293c
chienvd <chienvd@wini.vn>
2025-03-28 11:32:05 +0700

    6.2.7

--- dist/module/card/config.d.ts
+++ dist/module/card/config.d.ts
@@ -1,3 +1,6 @@
 export declare const regexUrlWithVariables: RegExp;
 export declare const regexGetVariableByThis: RegExp;
+export declare const regexGetVariables: RegExp;
+export declare const regexWatchDoubleQuote: RegExp;
+export declare const regexWatchSingleQuote: RegExp;
 //# sourceMappingURL=config.d.ts.map
