3974ecc71890f1aab9c73ae83e5d889838ca4e37
chienvd <chienvd@wini.vn>
2025-03-31 16:36:53 +0700

    fix progressbar

--- src/component/progress-bar/progress-bar.tsx
+++ src/component/progress-bar/progress-bar.tsx
@@ -19,16 +19,16 @@ export function ProgressBar({ id, status = ComponentStatus.INFOR, percent = 80, 
     const [openDetails, setOpenDetails] = useState(true)
 
     return progressBarOnly ?
-        <div className={styles["progress-bar-value"]} style={{ '--percent-color': percentColor, '--full-color': fullColor, '--percent': `${percent}%`, minWidth: "16rem", ...(style ?? {}) } as CSSProperties} /> :
-        <div id={id} className={`col ${styles["progress-bar-container"]} ${className}`} style={style ? { padding: progressBarOnly ? '0' : '1.6rem 2.4rem', ...style } : { padding: progressBarOnly ? '0' : '1.6rem 2.4rem' }}>
-            {(hideTitle || progressBarOnly) ? null : (title ?? <div className={`row ${styles["progress-bar-title"]}`}>
+        <div id={id} className={`${styles["progress-bar-value"]} ${className}`} style={{ '--percent-color': percentColor, '--full-color': fullColor, '--percent': `${percent}%`, minWidth: "16rem", ...(style ?? {}) } as CSSProperties} /> :
+        <div id={id} className={`col ${styles["progress-bar-container"]} ${className}`} style={style ? { padding: '1.6rem 2.4rem', ...style } : { padding: '1.6rem 2.4rem' }}>
+            {hideTitle ? null : (title ?? <div className={`row ${styles["progress-bar-title"]}`}>
                 <div className="heading-8">{titleText}</div>
                 <Winicon src={openDetails ? "fill/arrows/down-arrow" : "fill/arrows/up-arrow"} onClick={() => { setOpenDetails(!openDetails) }} />
             </div>)}
             {openDetails ? <div className={`row ${styles["progress-bar-tile"]}`}>
                 <div className={styles["progress-bar-value"]} style={{ '--percent-color': percentColor, '--full-color': fullColor, '--percent': `${percent}%`, ...(progressBarStyle ?? {}) } as CSSProperties}></div>
-                {progressBarOnly || status === ComponentStatus.INFOR ? null : <div className={`${styles["status-icon"]}`}>{getStatusIcon(status)}</div>}
-                {progressBarOnly ? null : <div className='label-4'>{percent}/100</div>}
+                {status === ComponentStatus.INFOR ? null : <div className={`${styles["status-icon"]}`}>{getStatusIcon(status)}</div>}
+                <div className='label-4'>{percent}/100</div>
             </div> : null}
         </div>
 }
