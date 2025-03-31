912eb4b3082fff2e4b736c84c21b990ba64ded9b
chienvd <chienvd@wini.vn>
2025-03-31 16:31:46 +0700

    fix progressbar

--- src/component/progress-bar/progress-bar.tsx
+++ src/component/progress-bar/progress-bar.tsx
@@ -18,15 +18,17 @@ export function ProgressBar({ id, status = ComponentStatus.INFOR, percent = 80, 
 }) {
     const [openDetails, setOpenDetails] = useState(true)
 
-    return <div id={id} className={`col ${styles["progress-bar-container"]} ${className}`} style={style ? { padding: progressBarOnly ? '0' : '1.6rem 2.4rem', ...style } : { padding: progressBarOnly ? '0' : '1.6rem 2.4rem' }}>
-        {(hideTitle || progressBarOnly) ? null : (title ?? <div className={`row ${styles["progress-bar-title"]}`}>
-            <div className="heading-8">{titleText}</div>
-            <Winicon src={openDetails ? "fill/arrows/down-arrow" : "fill/arrows/up-arrow"} onClick={() => { setOpenDetails(!openDetails) }} />
-        </div>)}
-        {openDetails ? <div className={`row ${styles["progress-bar-tile"]}`}>
-            <div className={styles["progress-bar-value"]} style={{ '--percent-color': percentColor, '--full-color': fullColor, '--percent': `${percent}%`, ...(progressBarStyle ?? {}) } as CSSProperties}></div>
-            {progressBarOnly || status === ComponentStatus.INFOR ? null : <div className={`${styles["status-icon"]}`}>{getStatusIcon(status)}</div>}
-            {progressBarOnly ? null : <div className='label-4'>{percent}/100</div>}
-        </div> : null}
-    </div>
+    return progressBarOnly ?
+        <div className={styles["progress-bar-value"]} style={{ '--percent-color': percentColor, '--full-color': fullColor, '--percent': `${percent}%`, minWidth: "16rem", ...(style ?? {}) } as CSSProperties} /> :
+        <div id={id} className={`col ${styles["progress-bar-container"]} ${className}`} style={style ? { padding: progressBarOnly ? '0' : '1.6rem 2.4rem', ...style } : { padding: progressBarOnly ? '0' : '1.6rem 2.4rem' }}>
+            {(hideTitle || progressBarOnly) ? null : (title ?? <div className={`row ${styles["progress-bar-title"]}`}>
+                <div className="heading-8">{titleText}</div>
+                <Winicon src={openDetails ? "fill/arrows/down-arrow" : "fill/arrows/up-arrow"} onClick={() => { setOpenDetails(!openDetails) }} />
+            </div>)}
+            {openDetails ? <div className={`row ${styles["progress-bar-tile"]}`}>
+                <div className={styles["progress-bar-value"]} style={{ '--percent-color': percentColor, '--full-color': fullColor, '--percent': `${percent}%`, ...(progressBarStyle ?? {}) } as CSSProperties}></div>
+                {progressBarOnly || status === ComponentStatus.INFOR ? null : <div className={`${styles["status-icon"]}`}>{getStatusIcon(status)}</div>}
+                {progressBarOnly ? null : <div className='label-4'>{percent}/100</div>}
+            </div> : null}
+        </div>
 }
