d5cc93c5d73339a3c75f0f6e286d22e05d6cd1e1
thuno <chienvd@wini.vn>
2025-06-14 10:55:28 +0700

    icon picker

---
+++ src/component/icon-picker/icon-picker.tsx
@@ -0,0 +1,42 @@
+import { CSSProperties, useEffect, useRef, useState } from "react"
+import { IconLibrary } from "./iconLibrary";
+import { Winicon } from "../wini-icon/winicon";
+
+interface IconPickerProps {
+    src?: string;
+    style?: CSSProperties;
+    onChange?: (src?: string) => void;
+    iconOnly?: boolean
+}
+
+export const IconPicker = (props: IconPickerProps) => {
+    const offsetRef = useRef<{ [p: string]: any }>(null)
+    const [value, setValue] = useState<string | undefined>()
+    const [isOpen, setIsOpen] = useState(false)
+
+    useEffect(() => {
+        setValue(props.src)
+    }, [props.src])
+
+    const onOpenIconLib = (ev: any) => {
+        if (isOpen) return null;
+        const rect = ev.target.closest("div").getBoundingClientRect()
+        let offset: any = {}
+        if (rect.bottom + 240 >= document.body.offsetHeight) offset.bottom = `calc(100dvh - ${rect.y}px + 2px)`
+        else offset.top = rect.bottom + 2
+        offsetRef.current = offset
+        setIsOpen(true)
+    }
+
+    return <>
+        <Winicon src={(value ?? "outline/user interface/setup-tools") as any} onClick={onOpenIconLib} />
+        {isOpen && <IconLibrary
+            onSelect={(src) => {
+                setValue(`${src.type}/${src.category}/${src.name}`)
+                setIsOpen(false)
+            }}
+            style={{ position: "fixed", zIndex: 2, ...offsetRef.current }}
+            onClose={() => { setIsOpen(false) }}
+        />}
+    </>
+}
---
+++ src/component/icon-picker/iconLibrary.tsx
@@ -0,0 +1,145 @@
+import styles from './iconLibrary.module.css'
+import { CSSProperties, useDeferredValue, useEffect, useRef, useState } from "react";
+import { useTranslation } from 'react-i18next';
+import { BaseDA } from '../../controller/config';
+import { TextField } from '../text-field/text-field';
+import { Winicon } from '../wini-icon/winicon';
+import { Button } from '../button/button';
+import { Text } from '../text/text';
+
+interface IconLibraryProps {
+    onSelect: (src: { [p: string]: any }) => void;
+    style?: CSSProperties;
+    onClose?: () => void;
+    className?: string;
+}
+
+export function IconLibrary({ onSelect, style = {}, onClose, className }: IconLibraryProps) {
+    const divRef = useRef<HTMLDivElement>(null)
+    const [filter, setFilter] = useState<undefined | string>();
+    const [searchValue, setSearchValue] = useState<string>("");
+    const search = useDeferredValue(searchValue, "")
+    const [showFilter, setOpenFilter] = useState(false);
+    const [categories, setCategories] = useState<string[]>([]);
+    const [data, setData] = useState<{ data: any[], totalCount?: number }>({ data: [], totalCount: undefined });
+    const [preview, setPreview] = useState<{ [p: string]: any } | null>(null);
+    const inputRef = useRef<any>(null)
+    const { t } = useTranslation()
+
+    useEffect(() => {
+        if (onClose) {
+            if (divRef.current) {
+                const onClickDropDown = (ev: any) => {
+                    if (ev.target === divRef.current || !divRef.current!.contains(ev.target)) onClose()
+                }
+                window.document.body.addEventListener("mousedown", onClickDropDown)
+                return () => {
+                    window.document.body.removeEventListener("mousedown", onClickDropDown)
+                }
+            }
+            return () => onClose()
+        }
+    }, [onClose])
+
+    const getIcons = async (page?: number) => {
+        let query = filter ? `type=${filter}` : ""
+        if (search.length) query += `${query.length ? "&" : ""}search=${search}`
+        if (page) query += `${query.length ? "&" : ""}page=${page}`
+        const response = await BaseDA.get(`https://redis.ktxgroup.com.vn/api/file/icon-library?${query}`)
+        if (response.code === 200) {
+            setData({ data: page && page > 1 ? [...data.data, ...response.data] : response.data, totalCount: response.totalCount })
+            if (page && page > 1) {
+                const newCate = response.category.filter((c: string) => !categories.includes(c))
+                if (newCate.length) setCategories([...categories, ...newCate])
+            } else setCategories(response.category ?? [])
+        }
+    }
+
+    useEffect(() => {
+        getIcons()
+    }, [filter, search])
+
+    return <div
+        ref={divRef}
+        className={`col ${styles['icon-library-container']} ${className ?? ""}`} style={style}
+        onScroll={(ev) => {
+            let scrollElement = ev.target as HTMLDivElement
+            if (Math.round(scrollElement.offsetHeight + scrollElement.scrollTop) >= (scrollElement.scrollHeight - 1) && data.totalCount && data.data.length < data.totalCount!) getIcons(Math.floor(data.data.length / 20) + 1)
+        }}
+    >
+        <div className={`row ${styles['search-container']}`}>
+            <TextField
+                ref={inputRef}
+                placeholder={t("search")}
+                className={`body-3 size32 ${styles['search-input']}`}
+                prefix={<Winicon src="outline/user interface/zoom" size="1.4rem" />}
+                onChange={(ev) => {
+                    if (ev.target.value.trim().length % 2 === 0) setSearchValue(ev.target.value.trim())
+                }}
+                onComplete={(ev: any) => ev.target.blur()}
+                onBlur={(ev) => {
+                    if (ev.target.value.trim().length >= 2) setSearchValue(ev.target.value.trim())
+                }}
+            />
+            <Button
+                prefix={<Winicon src='outline/user interface/setup-tools' color={filter ? "var(--primary-main-color)" : undefined} size={14} />}
+                className='button-text-3 size24'
+                label={`${filter ?? t("filter")}`}
+                style={{ width: "10.6rem", backgroundColor: filter ? "var(--primary-background)" : undefined, color: filter ? "var(--primary-main-color)" : undefined }}
+                suffix={filter ? <Winicon src={"outline/user interface/e-remove"} size={12} className="icon-button light" onClick={(ev: any) => {
+                    ev.preventDefault()
+                    ev.stopPropagation()
+                    setFilter(undefined)
+                }} /> : undefined}
+                onClick={() => { setOpenFilter(!showFilter) }}
+            />
+            {showFilter && <FilterDropdown
+                selected={filter}
+                onClose={() => { setTimeout(() => { setOpenFilter(false) }, 150) }}
+                onSelect={(value: string) => {
+                    setFilter(value)
+                    setOpenFilter(false)
+                }}
+            />}
+        </div>
+        {categories.map((cate) => {
+            return <div key={cate} className='col' style={{ gap: "0.8rem", padding: "0.8rem" }}>
+                <Text className='label-1'>{cate.slice(0, 1).toUpperCase() + cate.slice(1)}</Text>
+                <div className='row' style={{ flexWrap: "wrap", gap: "0.8rem", padding: "0.8 1.6rem", alignItems: "stretch" }}>
+                    {data.data.filter(e => e.category === cate).map((src: any, i: number) => <div key={src.name + src.id + i} className={`col col6 ${styles['icon-item']}`} onDoubleClick={() => { onSelect(src) }} onMouseOver={() => { setPreview(src) }}>
+                        <Winicon src={`${src.type}/${src.category}/${src.name}` as any} size={"2rem"} />
+                        <div className='label-5' style={{ width: "100%", wordBreak: "break-all" }}>{src.name}</div>
+                    </div>)}
+                </div>
+            </div>
+        })}
+        {preview && <div className={`row ${styles["preview-container"]}`}>
+            <Winicon src={`${preview.type}/${preview.category}/${preview.name}` as any} size={"3.2rem"} />
+            <div className='label-3' style={{ width: "100%", wordBreak: "break-all" }}>{preview.name}</div>
+        </div>}
+    </div>
+}
+
+const FilterDropdown = (props: { onClose: () => void, selected?: string, onSelect: (value: string) => void }) => {
+    const divRef = useRef<HTMLDivElement>(null)
+    useEffect(() => {
+        return () => props.onClose()
+    }, [])
+
+    useEffect(() => {
+        if (divRef.current) {
+            const onClickDropDown = (ev: any) => {
+                if (ev.target === divRef.current || !divRef.current!.contains(ev.target)) props.onClose()
+            }
+            window.document.body.addEventListener("mousedown", onClickDropDown)
+            return () => {
+                window.document.body.removeEventListener("mousedown", onClickDropDown)
+            }
+        }
+    }, [])
+    return <div ref={divRef} className={`col ${styles["filter-options"]}`}>
+        <button type="button" className={`${styles['option']} ${props.selected === "color" ? styles['selected'] : ""} heading-9`} onClick={() => { props.onSelect("color") }}>color</button>
+        <button type="button" className={`${styles['option']} ${props.selected === "fill" ? styles['selected'] : ""} heading-9`} onClick={() => { props.onSelect("fill") }}>fill</button>
+        <button type="button" className={`${styles['option']} ${props.selected === "outline" ? styles['selected'] : ""} heading-9`} onClick={() => { props.onSelect("outline") }}>outline</button>
+    </div>
+}
