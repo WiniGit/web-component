696b5f03bf2b79bb8795e74d6de4e51113aa4525
thuno <chienvd@wini.vn>
2025-06-14 10:56:31 +0700

    7.4.6

---
+++ dist/component/icon-picker/icon-picker.d.ts
@@ -0,0 +1,10 @@
+import { CSSProperties } from 'react';
+interface IconPickerProps {
+    src?: string;
+    style?: CSSProperties;
+    onChange?: (src?: string) => void;
+    iconOnly?: boolean;
+}
+export declare const IconPicker: (props: IconPickerProps) => import("react/jsx-runtime").JSX.Element;
+export {};
+//# sourceMappingURL=icon-picker.d.ts.map
