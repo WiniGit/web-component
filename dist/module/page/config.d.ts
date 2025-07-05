ffacad1131c117cf09e5cf07f33f1f934138dd28
thuno <thuno2698@gmail.com>
2025-07-05 17:37:33 +0700

    7.7.3

--- dist/module/page/config.d.ts
+++ dist/module/page/config.d.ts
@@ -1,4 +1,5 @@
 export declare const regexAvoidCls: RegExp;
+export declare const regexI18n: RegExp;
 export declare const avoidCls: string[];
 export declare const supportProperties: string[];
 //# sourceMappingURL=config.d.ts.map
