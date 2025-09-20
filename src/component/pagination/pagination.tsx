efa45a3826d03c855178e7ed9df374e7df98b0c7
Thuno <thuno2698@gmail.com>
2025-09-20 16:25:38 +0700

    pagination 8.0.95

--- src/component/pagination/pagination.tsx
+++ src/component/pagination/pagination.tsx
@@ -12,14 +12,15 @@ interface Props {
     id?: string,
     currentPage: number,
     itemPerPage: number,
-    totalItem: number,
-    onChangePage: Function,
+    totalItem?: number,
+    onChangePage?: Function,
     hideGoToPage?: boolean,
     hidePageSize?: boolean,
     style?: CSSProperties,
+    className?: string
 }
 
-export function Pagination({ id, currentPage, itemPerPage = 10, totalItem, onChangePage, hidePageSize = false, hideGoToPage = false, style }: Props) {
+export function Pagination({ itemPerPage = 10, ...props }: Props) {
     const goToPageRef = useRef<TextFieldRef>(null)
     const { t } = useTranslation()
     const popupRef = useRef<any>(null)
@@ -27,16 +28,16 @@ export function Pagination({ id, currentPage, itemPerPage = 10, totalItem, onCha
     useEffect(() => {
         if (goToPageRef.current) {
             const _inputPage = goToPageRef.current.inputElement
-            if (_inputPage) _inputPage.value = currentPage.toString()
+            if (_inputPage) _inputPage.value = props.currentPage.toString()
         }
-    }, [currentPage])
+    }, [props.currentPage])
 
-    if (currentPage > 1 && (totalItem === 0 || (Math.floor(totalItem / itemPerPage) + (totalItem % itemPerPage === 0 ? 0 : 1)) < currentPage)) {
-        onChangePage(1, itemPerPage);
+    if (props.currentPage > 1 && (!props.totalItem || (Math.floor(props.totalItem / itemPerPage) + (props.totalItem % itemPerPage === 0 ? 0 : 1)) < props.currentPage)) {
+        props.onChangePage?.(1, itemPerPage);
         return <div></div>;
-    } else if (totalItem > 0) {
-        return <div id={id} className={`${styles['custom-pagination']} row`} style={style}>
-            {hidePageSize ? null : <div className="row" style={{ gap: '0.8rem' }}>
+    } else if (props.totalItem) {
+        return <div id={props.id} className={`${styles['custom-pagination']} row ${props.className ?? ""}`} style={props.style}>
+            {!props.hidePageSize && <div className="row" style={{ gap: '0.8rem' }}>
                 <Popup ref={popupRef} />
                 <Select1
                     value={itemPerPage}
@@ -45,20 +46,20 @@ export function Pagination({ id, currentPage, itemPerPage = 10, totalItem, onCha
                     style={{ width: '6.8rem' }}
                     suffix={<Winicon src={"fill/arrows/down-arrow"} size={12} />}
                     onChange={(ev: any) => {
-                        onChangePage(currentPage, isNaN(parseInt(ev.id)) ? itemPerPage : parseInt(ev.id));
+                        props.onChangePage?.(props.currentPage, isNaN(parseInt(ev.id)) ? itemPerPage : parseInt(ev.id));
                     }}
                 />
-                <Text className="body-3">{t("ofItems", { totalItem: totalItem })}</Text>
+                <Text className="body-3">{t("ofItems", { totalItem: props.totalItem })}</Text>
             </div>}
             <div style={{ flex: 1 }} />
             <ReactPaginate
                 onPageChange={(ev) => {
-                    onChangePage(ev.selected + 1, itemPerPage);
+                    props.onChangePage?.(ev.selected + 1, itemPerPage);
                 }}
-                forcePage={currentPage - 1}
+                forcePage={props.currentPage - 1}
                 breakClassName="row button-text-3"
                 breakLabel="..."
-                pageCount={Math.ceil(totalItem / itemPerPage)}
+                pageCount={Math.ceil(props.totalItem / itemPerPage)}
                 previousClassName="row"
                 previousLabel={<Winicon src={"fill/arrows/left-arrow"} size={"1.4rem"} />}
                 nextClassName="row"
@@ -67,11 +68,11 @@ export function Pagination({ id, currentPage, itemPerPage = 10, totalItem, onCha
                 pageClassName="row button-text-3"
                 activeClassName={styles['active']}
                 hrefBuilder={(pageIndex: number) =>
-                    pageIndex >= 1 && pageIndex <= Math.ceil(totalItem / itemPerPage) ? `/page/${pageIndex}` : '#'
+                    pageIndex >= 1 && pageIndex <= Math.ceil(props.totalItem! / itemPerPage) ? `/page/${pageIndex}` : '#'
                 }
                 renderOnZeroPageCount={null}
             />
-            {!hideGoToPage && <>
+            {!props.hideGoToPage && <>
                 <div style={{ height: '1.6rem', backgroundColor: "var(--neutral-bolder-border-color, light-dark(#D7D7DB, #494950))", width: 1 }} />
                 <Text className="label-3">{t("go")} {t("page").toLowerCase()}</Text>
                 <TextField
@@ -81,12 +82,12 @@ export function Pagination({ id, currentPage, itemPerPage = 10, totalItem, onCha
                     type="number"
                     onBlur={(ev) => {
                         const _tmp = ev.target.value.trim().length ? parseInt(ev.target.value.trim()) : undefined
-                        if (_tmp && !isNaN(_tmp) && _tmp > 0 && _tmp <= Math.ceil(totalItem / itemPerPage)) {
-                            onChangePage(_tmp, itemPerPage)
+                        if (_tmp && !isNaN(_tmp) && _tmp > 0 && _tmp <= Math.ceil(props.totalItem! / itemPerPage)) {
+                            props.onChangePage?.(_tmp, itemPerPage)
                         } else ev.target.value = ""
                     }}
                 />
             </>}
         </div>
-    } else return <div id={id} />
+    } else return <div id={props.id} style={props.style} className={props.className} />
 }
