4ec1ca84f74ac9c9cb5ff9c5aa4ff7327da2c398
thuntmac <thunt@wini.vn>
2026-01-08 13:29:03 +0700

    8.3.70

--- src/component/pagination/pagination.tsx
+++ src/component/pagination/pagination.tsx
@@ -36,20 +36,22 @@ export function Pagination({ itemPerPage = 10, ...props }: Props) {
         return <div></div>;
     } else if (props.totalItem) {
         return <div id={props.id} className={`${styles[props.simpleStyle ? 'simple-custom-pagination' : 'custom-pagination']} row ${props.className ?? ""}`} style={props.style}>
-            {!props.hidePageSize && <div className="row" style={{ gap: '0.8rem' }}>
-                <Select1
-                    value={itemPerPage}
-                    options={[10, 20, 50, 100, 200].map((item, _) => { return { id: item, name: `${item}` } })}
-                    className="body-3 size24"
-                    style={{ width: '6.8rem' }}
-                    suffix={<Winicon src={"fill/arrows/down-arrow"} size={12} />}
-                    onChange={(ev: any) => {
-                        props.onChangePage?.(props.currentPage, isNaN(parseInt(ev.id)) ? itemPerPage : parseInt(ev.id));
-                    }}
-                />
-                <Text className="body-3">{t("ofItems", { totalItem: props.totalItem })}</Text>
-            </div>}
-            <div style={{ flex: 1 }} />
+            {!props.hidePageSize && <>
+                <div className="row" style={{ gap: '0.8rem' }}>
+                    <Select1
+                        value={itemPerPage}
+                        options={[10, 20, 50, 100, 200].map((item, _) => { return { id: item, name: `${item}` } })}
+                        className="body-3 size24"
+                        style={{ width: '6.8rem' }}
+                        suffix={<Winicon src={"fill/arrows/down-arrow"} size={12} />}
+                        onChange={(ev: any) => {
+                            props.onChangePage?.(props.currentPage, isNaN(parseInt(ev.id)) ? itemPerPage : parseInt(ev.id));
+                        }}
+                    />
+                    <Text className="body-3">{t("ofItems", { totalItem: props.totalItem })}</Text>
+                </div>
+                <div style={{ flex: 1 }} />
+            </>}
             <ReactPaginate
                 onPageChange={(ev) => {
                     props.onChangePage?.(ev.selected + 1, itemPerPage);
