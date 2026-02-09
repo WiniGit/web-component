00c274568cb49fdcd36c33ebfbfd050e48ccf404
thuntmac <thunt@wini.vn>
2026-02-09 15:22:12 +0700

    init

--- src/module/card/config.tsx
+++
@@ -1,7 +0,0 @@
-export const regexUrlWithVariables = /^(https?:\/\/)?([\w.-]+)?(:\d+)?(\/(\$\{[\w.]+\}|[\w.-]*))*$/;
-export const regexGetVariableByThis = /\${this\.(\w+)}/;
-export const replaceVariableByThis = /\${this\.(\w+)}/g;
-export const regexGetVariables = /\${([^}]*)}/;
-export const replaceVariables = /\$\{([\s\S]*)\}/g; 
-export const regexEmptyKeyController = /@([^:]+):(?:{empty}|{notempty})/;
-export const replaceEmptyKeyController = /@([^:]+):(?:{empty}|{notempty})/g;
