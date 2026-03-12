1f39a0fd184b35ad845f9eaf8f24fd629b8279f4
thuntmac <thunt@wini.vn>
2026-03-12 18:52:07 +0700

    8.4.61

--- src/controller/config.tsx
+++ src/controller/config.tsx
@@ -207,7 +207,6 @@ export class BaseDA {
 
 export class CkEditorUploadAdapter {
     loader: any;
-    xhr: XMLHttpRequest | undefined;
 
 
     constructor(loader: any) {
@@ -217,92 +216,26 @@ export class CkEditorUploadAdapter {
 
     // Starts the upload process.
     upload() {
-        return this.loader.file
-            .then((file: any) => new Promise((resolve, reject) => {
-                this._initRequest();
-                this._initListeners(resolve, reject, file);
-                this._sendRequest(file);
-            }));
+        return this.loader.file.then((file: any) => new Promise((resolve, reject) => {
+            this._sendRequest(file, resolve, reject);
+        }));
     }
 
-    // Aborts the upload process.
-    abort() {
-        if (this.xhr) {
-            this.xhr.abort();
-        }
-    }
-
-    // Initializes the XMLHttpRequest object using the URL passed to the constructor.
-    async _initRequest() {
-        const xhr = this.xhr = new XMLHttpRequest();
-        // Note that your request may look different. It is up to you and your editor
-        // integration to choose the right communication channel. This example uses
-        // a POST request with JSON as a data structure but your configuration
-        // could be different.
-        let _headers: any = await getHeaders()
-        xhr.open('POST', ConfigData.url + 'file/uploadfiles', true);
-        xhr.setRequestHeader("Authorization", _headers.Authorization);
-        xhr.setRequestHeader("pid", ConfigData.pid);
-        xhr.setRequestHeader("Content-Type", "multipart/form-data");
-        xhr.responseType = 'json';
-    }
-
-    // Initializes XMLHttpRequest listeners.
-    _initListeners(resolve: { (value: unknown): void; (arg0: { default: string }): void }, reject: { (reason?: any): void; (arg0: string | undefined): any }, file: { name: any; }) {
-        const xhr = this.xhr;
-        const loader = this.loader;
-        const genericErrorText = `Couldn't upload file: ${file.name}.`;
-
-        xhr?.addEventListener('error', () => reject(genericErrorText));
-        xhr?.addEventListener('abort', () => reject());
-        xhr?.addEventListener('load', () => {
-            const response = xhr.response;
-
-            // This example assumes the XHR server's "response" object will come with
-            // an "error" which has its own "message" that can be passed to reject()
-            // in the upload promise.
-            //
-            // Your integration may handle upload errors in a different way so make sure
-            // it is done properly. The reject() function must be called when the upload fails.
-            if (!response || response.error) {
-                return reject(response && response.error ? response.error.message : genericErrorText);
+    async _sendRequest(file: File, resolve: { (value: unknown): void; (arg0: { default: string }): void }, reject: { (arg0: string): void }) {
+        BaseDA.uploadFiles([file]).then((res: any) => {
+            if (res?.length) {
+                resolve({
+                    default: res[0].Url
+                })
+            } else {
+                ToastMessage.errors("Failed to upload file")
+                reject("Failed to upload file")
+                console.error(`Failed to upload file: ${res?.message}`)
             }
-
-            // If the upload is successful, resolve the upload promise with an object containing
-            // at least the "default" URL, pointing to the image on the server.
-            // This URL will be used to display the image in the content. Learn more in the
-            // UploadAdapter#upload documentation.
-            resolve({
-                default: ConfigData.imgUrlId + response.data[0].Id
-            });
-        });
-
-        // Upload progress when it is supported. The file loader has the #uploadTotal and #uploaded
-        // properties which are used e.g. to display the upload progress bar in the editor
-        // user interface.
-        if (xhr?.upload) {
-            xhr.upload.addEventListener('progress', (evt: { lengthComputable: any; total: any; loaded: any; }) => {
-                if (evt.lengthComputable) {
-                    loader.uploadTotal = evt.total;
-                    loader.uploaded = evt.loaded;
-                }
-            });
-        }
-    }
-
-    // Prepares the data and sends the request.
-    _sendRequest(file: string | Blob) {
-        // Prepare the form data.
-        const data = new FormData();
-
-        data.append('files', file);
-
-        // Important note: This is the right place to implement security mechanisms
-        // like authentication and CSRF protection. For instance, you can use
-        // XMLHttpRequest.setRequestHeader() to set the request headers containing
-        // the CSRF token generated earlier by your application.
-
-        // Send the request.
-        this.xhr?.send(data);
+        }).catch(err => {
+            ToastMessage.errors("Failed to upload file")
+            reject("Failed to upload file")
+            console.error(`Failed to upload file: ${err.message}`)
+        })
     }
 }
